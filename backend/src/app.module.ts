0d579b89ae4ecd3a81ab8552fee5e6d124cc5735
Margesh Modi <margeshmodi93@gmail.com>
2025-01-30 17:16:23 +0530

    event_category post api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './config/data-source';
 import { EventsModule } from './events/events.module';
 import { CategoriesModule } from './categories/categories.module';  // Ensure CategoriesModule is imported
+import { EventCategoryModule } from './categories/event_category/event_category.module'
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(TypeOrmConfig),
     EventsModule,  
     CategoriesModule,  
+    EventCategoryModule
   ],
 })
 export class AppModule {}
