d4f7360f08c64ea13f668dc6fd25c5e45dcacdf2
Abhishek-jii <sharmaji28052003@gmail.com>
2025-01-31 12:13:09 +0530

    commit

--- frontend/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['plus.unsplash.com','race-registration-cdn.indiarunning.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
