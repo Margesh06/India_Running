990e73b6c2e627d2ba70620ce6dcc85a4681c37f
Margesh Modi <margeshmodi93@gmail.com>
2025-02-03 12:32:48 +0530

    updated organiser table

--- backend/src/entities/organiser.entity.ts
+++ backend/src/entities/organiser.entity.ts
@@ -1,16 +1,38 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
 
 @Entity("Organiser")
 export class Organiser {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  name: string;
+  @Column({ name: "fname" })
+  firstName: string;
+
+  @Column({ name: "lname" })
+  lastName: string;
 
   @Column()
   phone_no: number;
 
+  @Column({ name: "org_name" })
+  organizationName: string;
+
   @Column()
   email: string;
+
+  @Column({ type: "text" })
+  address: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @Column({ type: "text" })
+  pincode: string;
+
+  @Column({ name: "pan_card", type: "text" })
+  panCard: string;
+
 }
